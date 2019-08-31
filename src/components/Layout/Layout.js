eb86b567ca0494d84651fc4e0f67544cc948fcea
Geraldo Silva <gmagela@kenzan.com>
2019-08-31 16:09:12 -0400

    first commit - react burger app

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
+const layout = props => (
+  <>
+    <Toolbar />
+    <main className={classes.Content}>{props.children}</main>
+  </>
+);
+
+export default layout;
---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const navigationItems = props => (
+  <ul>
+    <li>
+      <a href="/">A Link</a>
+    </li>
+  </ul>
+);
+
+export default navigationItems;
